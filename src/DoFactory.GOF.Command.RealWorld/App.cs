8d70e188ed2426f55ae37901d0a7133f1dad3147
Felipe Mendes <mendesfelipe00@gmail.com>
2017-04-07 13:22:17 -0300

    Initial commit

---
+++ src/DoFactory.GOF.Command.RealWorld/App.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoFactory.GOF.Command.RealWorld
+{
+    class App
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ src/DoFactory.GOF.Iterator.RealWorld/App.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoFactory.GOF.Iterator.RealWorld
+{
+    class App
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
